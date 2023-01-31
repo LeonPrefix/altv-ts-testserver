458c2dd10031f0f2916c0736f55a907532ec5636
LeonPrefix <herrprefix@gmail.com>
2023-01-31 23:54:11 +0100

    removed typing

--- resources/test/src/server/server.ts
+++ resources/test/src/server/server.ts
@@ -1,6 +1,5 @@
-import * as alt from 'alt-server';
 import * as chat from 'chat';
 
-chat.registerCmd('test', (player: alt.Player, args: string[]) => {
+chat.registerCmd('test', (player, args) => {
   chat.send(player, 'test');
 });
